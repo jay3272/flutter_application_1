3ce4e0151c3250c221fba8904414288d6d126b98
吳介 <jaywu3272@gmail.com>
2025-03-04 14:41:00 +0800

    class 2: add button

--- lib/main.dart
+++ lib/main.dart
@@ -39,6 +39,14 @@ class MyHomePage extends StatelessWidget {
         children: [
           Text('A random AWESOME idea:'),
           Text(appState.current.asLowerCase),
+
+          // ↓ Add this.
+          ElevatedButton(
+            onPressed: () {
+              print('button pressed!');
+            },
+            child: Text('Next'),
+          ),
         ],
       ),
     );
